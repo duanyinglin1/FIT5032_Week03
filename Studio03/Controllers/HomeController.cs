af632394d36026b5d2353b66e56b49196aabbeab
duanyinglin1 <wobushidyl@gmail.com>
2023-08-20 15:40:18 +1000

    Add project files.

---
+++ Studio03/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Studio03.Models.HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Studio03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           //ViewBag.Message = "Your application description page.";
+
+            // Instance of Hello object
+            Hello hello = new Hello();
+            ViewBag.Message = hello.GetHello();
+
+            ExampleDictionary exampleDictionary = new ExampleDictionary();
+            exampleDictionary.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ Studio03/Models/HelloWorld/Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Studio03.Models.HelloWorld
+{
+    public class Hello
+    {
+        public String GetHello() {
+            return "Hi, Hello World!";
+        }
+    }
+}
